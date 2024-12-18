adca45b6dba65a689175c4992be75bd5b76f5ba8
msaif2729 <saiiifansari@gmail.com>
2024-12-18 21:32:03 +0530

    Issue Resolved1

--- config/app.js
+++ config/app.js
@@ -6,12 +6,16 @@ const eamilRouter = require('../routes/emailRouter');
 const app = express();
 
 app.use(express.json());
+
 app.use(cors({
-    origin:['http://localhost:3000','https://prime-workers.vercel.app'],
-    method : ['GET','POST','PUT','DELETE']
+    origin: ['http://localhost:3000', 'https://prime-workers.vercel.app'], // Allowed origins
+    methods: ['GET', 'POST', 'PUT', 'DELETE', 'OPTIONS'], // Allowed HTTP methods
+    allowedHeaders: ['Content-Type', 'Authorization'], // Allowed headers
+    credentials: true, // Allow cookies if needed
 }));
 
-app.options('*', cors());
+app.options('*', cors()); // Handle preflight requests
+
 
 app.get('/', (req, res) => {
     res.send('API is working!');
