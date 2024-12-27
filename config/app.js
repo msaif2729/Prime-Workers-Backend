640afccf50b2a3ed8f6d7f2215dbd4fbed46bb8b
msaif2729 <saiiifansari@gmail.com>
2024-12-27 11:04:16 +0530

    Some Changes

--- config/app.js
+++ config/app.js
@@ -8,7 +8,7 @@ const app = express();
 app.use(express.json());
 
 app.use(cors({
-    origin: ['https://primeworkers.in','http://localhost:3000', 'https://prime-workers.vercel.app'], // Allowed origins
+    origin: ['https://www.primeworkers.in','https://primeworkers.in','http://localhost:3000', 'https://prime-workers.vercel.app'], // Allowed origins
     methods: ['GET', 'POST', 'PUT', 'DELETE', 'OPTIONS'], // Allowed HTTP methods
     allowedHeaders: ['Content-Type', 'Authorization'], // Allowed headers
     credentials: true, // Allow cookies if needed
